a2a90103e114d7a2eeb6583aa14ae552153113e5
Alisson Amarante <alisson.amarante@ccc.ufcg.edu.br>
2026-08-08 22:33:57 -0300

    Ajustando vite-env

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
-import.meta.dirname
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [
@@ -15,4 +14,3 @@ export default defineConfig({
     },
   },
 });
-
